f7c6f8d41f16ae2b24648eb3c6f904cfce2a8aab
Choisungrim <wowilos@naver.com>
2025-04-14 10:30:22 +0900

    mqtt 및 query-regisry update

--- src/common/adapter/mqtt.module.ts
+++ src/common/adapter/mqtt.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MqttSubscriber } from './mqtt.subscriber';
 import { MqttPublisher } from './mqtt.publisher.service';
-import { QueryRegistry } from '../utils/query/query-registry.service';
 import { CacheModule } from '../utils/cache/cache.module';
 
 @Module({
   imports: [CacheModule],
   controllers: [MqttSubscriber],
-  providers: [MqttPublisher, QueryRegistry],
+  providers: [MqttPublisher],
   exports: [MqttPublisher],
 })
 export class MqttModule {}
--- src/common/utils/query/query-registry.module.ts
+++ src/common/utils/query/query-registry.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { QueryRegistry } from './query-registry.service';
 
+@Global()
 @Module({
   providers: [QueryRegistry],
   exports: [QueryRegistry],
